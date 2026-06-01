9774942414bbb901e4d70e3c63a3bf1f27c3d9fc
namanpateriya <pateriyanaman@gmail.com>
2026-06-01 19:24:35 +0530

    Create resources.py

---
+++ app/server/resources.py
@@ -0,0 +1,29 @@
+PROJECT_STATUS = {
+    "project_name": "Digital Banking Platform",
+    "health": "Amber",
+    "timeline": "3 weeks delayed",
+    "summary": (
+        "Vendor dependency delays are "
+        "impacting delivery timelines."
+    )
+}
+
+
+PROJECT_RISKS = [
+    {
+        "risk_id": "R1",
+        "description": "Vendor dependency delay",
+        "severity": "High"
+    },
+    {
+        "risk_id": "R2",
+        "description": "Resource bandwidth constraints",
+        "severity": "Medium"
+    }
+]
+
+
+PROJECT_TIMELINE = {
+    "planned_release": "2026-09-30",
+    "forecast_release": "2026-10-21"
+}
